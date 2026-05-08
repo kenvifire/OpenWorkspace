c9cde11a5d4abfa01971e40538ed009022b616f8
Kenvi Zhu <kenvifly@gmail.com>
2026-05-08 17:10:50 +0800

    feat: add NotificationsController (list, mark-read, mark-all-read)

--- apps/api/src/modules/notifications/notifications.controller.ts
+++ apps/api/src/modules/notifications/notifications.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Patch, Param, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
+import { NotificationsService } from './notifications.service';
+import type { User } from '@prisma/client';
 
+@ApiTags('notifications')
+@ApiBearerAuth('jwt')
+@UseGuards(JwtAuthGuard)
 @Controller('notifications')
-export class NotificationsController {}
+export class NotificationsController {
+  constructor(private readonly service: NotificationsService) {}
+
+  @Get()
+  findAll(@CurrentUser() user: User) {
+    return this.service.findAll(user.id);
+  }
+
+  @Patch(':id/read')
+  markRead(@Param('id') id: string, @CurrentUser() user: User) {
+    return this.service.markRead(user.id, id);
+  }
+
+  @Patch('read-all')
+  markAllRead(@CurrentUser() user: User) {
+    return this.service.markAllRead(user.id);
+  }
+}
